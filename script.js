5c4a119c88941c2e8435407c93cde1b6bb05bcd9
Bryan Nathaniel <bryan2ndacc@gmail.com>
2023-10-20 21:41:04 +0800

    javascript comment remove

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-//Animation through container
-// document.addEventListener("DOMContentLoaded", function () {
-//     const con = document.querySelectorAll(".container");
-
-//     const observer = new IntersectionObserver(entries => {
-//         entries.forEach(entry => {
-//             entry.target.classList.toggle("show", entry.isIntersecting)
-//             if (entry.isIntersecting) observer.unobserve(entry.target)
-//         });
-//     }, {
-//         threshold: 0, // Add your desired threshold here (0.5 for 50% of the element in the viewport)
-//     });
-
-//     con.forEach(con => {
-//         observer.observe(con);
-//     });
-// });
-
-
 // https://www.youtube.com/watch?v=2IbRtjez6ag
 // show1
 
